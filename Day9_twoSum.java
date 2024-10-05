94d83f45c2e5f770814a73759b4033a986b7f463
wasiahmad <alexxwasi95@gmail.com>
2024-10-05 19:42:49 +0530

    Day9 Solution Two Sum

---
+++ Day9_twoSum.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+
+/**
+ * Day9_twoSum
+ */
+import java.util.*;
+public class Day9_twoSum {
+
+    public static int[] twoSum(int[] nums, int target) {
+       HashMap<Integer, Integer> map = new HashMap<>();
+       for (int  i = 0; i < nums.length; i++) {
+            int findMe = target - nums[i];
+            if(map.containsKey(findMe) ){
+                return new int[]{i, map.get(findMe)};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = twoSum(nums, target);
+        System.out.println(result);
+    }
+}
