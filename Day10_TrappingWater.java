a211f85f6cd4ce602f1e7cc1cd9cde87fd5cff8d
wasiahmad <alexxwasi95@gmail.com>
2024-10-06 19:37:08 +0530

    Day10 Solution Trapped Water

---
+++ Day10_TrappingWater.java
@@ -0,0 +1,28 @@
+/**
+ * Day10_TrappingWater
+ */
+public class Day10_TrappingWater {
+
+    public static int trap(int[] height) {
+        int left = 0, right = height.length - 1;
+        int maxLeft = height[left], maxRight = height[right];
+        int water = 0;
+        while (left < right) {
+            if (maxLeft < maxRight) {
+                left++;
+                maxLeft = Math.max(maxLeft, height[left]);
+                water += maxLeft - height[left];
+            } else {
+                right--;
+                maxRight = Math.max(maxRight, height[right]);
+                water += maxRight - height[right];
+            }
+        }
+        return water;
+    }
+
+    public static void main(String[] args) {
+        int[] height = {0,1,0,2,1,0,1,};
+        System.out.println(trap(height));
+    }
+}
