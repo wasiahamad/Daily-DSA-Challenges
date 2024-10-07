33bc4dbf6c45cc644948c4fc9a836f421f592eb8
wasiahmad <alexxwasi95@gmail.com>
2024-10-07 15:21:25 +0530

    Day11 Maximum SubArray

---
+++ Day11_MaxSubArray.java
@@ -0,0 +1,18 @@
+public class Day11_MaxSubArray {
+    public static int maxSubAarrySum(int arr[]) {
+        int maxSum = Integer.MIN_VALUE;
+        int currSum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            currSum += arr[i];
+            if (currSum < 0) {
+                currSum = 0;
+            }
+            maxSum = Math.max(maxSum, currSum);
+        }
+        return maxSum;
+    }
+    public static void main(String[] args) {
+        int arr[] = {-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println(maxSubAarrySum(arr));
+    }
+}
