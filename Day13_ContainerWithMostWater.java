62b77ca57f8602a0bca2313c491a0945a0bc1755
wasiahmad <alexxwasi95@gmail.com>
2024-11-05 12:51:31 +0530

    Day 13 Solution Container with Most Water

--- Day13_ContainerWithMostWater.java
+++ Day13_ContainerWithMostWater.java
@@ -1,6 +1,23 @@
 public class Day13_ContainerWithMostWater {
+    public static int mostWater(int arr[]) {
+        int maxWater = 0;
+        int lf = 0;
+        int rt = arr.length - 1;
+        while (lf < rt) {
+            int ht = Math.min(arr[lf], arr[rt]);
+            int wt = rt - lf;
+            maxWater = Math.max(maxWater, ht * wt);
+            if (arr[lf] < arr[rt]) {
+                lf++;
+            } else {
+                rt--;
+            }
+        }
+        return maxWater;
+    }
 
     public static void main(String[] args) {
-        
+        int height[] = {1,8,6,2,5,4,8,3,7} ;
+        System.out.println(mostWater(height));
     }
 }
