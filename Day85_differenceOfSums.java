0b0398eb6d05f4402bf26ba5358c64c6bfa364c8
wasiahmad <alexxwasi95@gmail.com>
2025-05-27 18:58:46 +0530

    Day85_differenceOfSums

---
+++ Day85_differenceOfSums.java
@@ -0,0 +1,21 @@
+public class Day85_differenceOfSums {
+    public int differenceOfSums(int n, int m) {
+        int sumDivisible = 0;
+        int sumNotDivisible = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (i % m == 0) {
+                sumDivisible += i;
+            } else {
+                sumNotDivisible += i;
+            }
+        }
+
+        return sumNotDivisible - sumDivisible;
+    }
+
+    // public static void main(String[] args) {
+    //     Day85_differenceOfSums solution = new Day85_differenceOfSums();
+    //     System.out.println(solution.differenceOfSums(10, 3)); // Example usage
+    // }
+}
