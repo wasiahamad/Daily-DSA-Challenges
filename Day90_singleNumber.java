65f70623a7a7469a7d53a0d5805acadeeaab8a79
wasiahmad <alexxwasi95@gmail.com>
2025-08-05 17:38:41 +0530

    Day 90 singleNumber

---
+++ Day90_singleNumber.java
@@ -0,0 +1,14 @@
+public class Day90_singleNumber {
+    // function to find the single number
+    public static int singleNumber(int[] nums) {
+        int result = 0;
+        for (int i = 0; i < nums.length; i++) {
+            result ^= nums[i];
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        int[] nums = {4, 1, 2, 1, 2};
+        System.out.println(singleNumber(nums));
+    }
+}
